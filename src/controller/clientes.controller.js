f415cd41cb8d41810dcc0b5d4b6316677528793a
Dayoripa <daulinri@gmail.com>
2021-09-26 23:52:58 -0500

    Adding metodo Put, Get y Delete

--- src/controller/clientes.controller.js
+++ src/controller/clientes.controller.js
@@ -1,17 +1,12 @@
 const Cliente = require("./../models/Cliente");
 
-exports.obtenerClientes = (req, res)=>{
-    const clientes = [
-        {
-            "nombre": "Daulin",
-            "correo": "daulin@gmail.com"
-        },
-        {
-          "nombre": "Lucas",
-          "correo": "lucas@gmail.com"
-        },
-    ]
-    res.json(clientes)
+exports.obtenerClientes = async(req, res)=>{
+    try {
+        const clientes = await Cliente.find();
+        res.json(clientes);
+    } catch (error) {
+        res.json(error)
+    }
 };
 
 exports.agregarClientes = async(req, res) =>{
@@ -40,15 +35,52 @@ exports.agregarClientes = async(req, res) =>{
     }
 };
 
-exports.actualizarClientes = (req, res) =>{
-    const id = req.params.id;
-    console.log(id)
-    res.json("id recibido para actualizar")
+exports.actualizarClientes = async(req, res) =>{
+
+    try {
+        const id = req.params.id;
+        const data = req.body;
+
+    if(id && data){
+        await Cliente.findByIdAndUpdate(id, data);
+        res.json({
+            status: 'ok',
+            code: 200,
+            message: 'Cliente actualizado'
+        });
+    }else{
+        res.json({
+            status: 'error',
+            code: 400,
+            message: 'Cliente no se pudo actualizar'
+        })
+    }
+        
+    } catch (error) {
+        res.json({
+            status: 'error',
+            code: 400,
+            message: 'Cliente no se pudo actualizar'
+        })
+    }
+    
 };
 
-exports.eliminarClientes = (req, res) =>{
-    const id = req.params.id;
-    console.log(id)
-    res.json("id recibido para eliminar")
+exports.eliminarClientes = async(req, res) =>{
+    try {
+        const id = req.params.id;
+        const eliminado = await Cliente.findByIdAndDelete(id)
+        res.json({
+            status: 'ok',
+            code: 200,
+            message: 'Cliente Eliminado'
+        });
+    } catch (error) {
+        res.json({
+            status: 'error',
+            code: 400,
+            message: 'No se pudo eliminar'
+        })
+    }
 
 }
